541fc8e6e38b1072377a82ef1b76754c0b815531
Steto Javellana <javellanasteto99@gmail.com>
2024-10-09 23:41:14 +0800

    add components Footer and Navigation

---
+++ components/Footer.vue
@@ -0,0 +1,73 @@
+<template>
+  <VFooter absolute class="pa-0">
+    <VContainer fluid class="px-6 px-md-16">
+      <VRow  class="py-8">
+        <VCol cols="12" md="6">
+          <div>
+            <div>
+              <VImg src="/img/logo.png" max-height="125" max-width="125" />
+            </div>
+            <p class="text-body-2 text-caption mt-2">
+              OOBRA is a platform that empowers digital workers by allowing them
+              to take advantage of an agency's benefits, while simultaneously
+              providing Clients a reliable channel for effortless recruitment;
+              Grow your portfolio with real world experiences through Oobra, the
+              Digital Worker's platform.
+            </p>
+          </div>
+        </VCol>
+        <VCol cols="12" md="3">
+          <div >
+            <div class="font-weight-bold mb-2">Contact Us</div>
+            <div class="text-body-2 text-caption">
+              <span class="font-weight-bold">Email:</span> info@oobra.com
+            </div>
+            <div class="text-body-2 text-caption">
+              <span class="font-weight-bold">Phone:</span> (123) 456-7890
+            </div>
+          </div>
+        </VCol>
+        <VCol cols="12" md="3">
+          <div>
+            <div class="font-weight-bold mb-2">Contact with us</div>
+            <v-btn
+              v-for="item in icons"
+              :key="item"
+              :icon="item.icon"
+              variant="text"
+              class="text-caption ml-n4"
+              :color="item.color"
+            ></v-btn></div
+        ></VCol>
+      </VRow>
+      <VDivider color="white-thirdy" thickness="2"></VDivider>
+      <div class="py-6 text-caption text-center">
+        Copyright &#9400;
+        {{ new Date().getFullYear() }} OOBRA. All rights reserved.
+      </div>
+    </VContainer>
+  </VFooter>
+</template>
+
+<script setup lang="ts">
+import { useDisplay } from "vuetify";
+
+const icons = [
+  {
+    icon: "mdi-facebook",
+    color: "blue",
+  },
+  {
+    icon: "mdi-twitter",
+    color: "blue",
+  },
+  {
+    icon: "mdi-linkedin",
+    color: "blue",
+  },
+  {
+    icon: "mdi-instagram",
+    color: "blue",
+  },
+];
+</script>
---
+++ components/Navigation.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <VAppBar
+      :elevation="2"
+      height="70"
+      class="px-6 px-md-16"
+    >
+      <VToolbarTitle class="mx-0">
+        <NuxtLink to="/">
+          <VImg
+            src="/img/logo.png"
+            cover
+            max-height="225"
+            max-width="225"
+            alt="OOBRA"
+          />
+        </NuxtLink>
+      </VToolbarTitle>
+      <VSpacer />
+      <VAppBarNavIcon @click.stop="drawer = !drawer" class="d-flex d-md-none">
+        <VIcon size="x-large" color="primary"> mdi-reorder-horizontal </VIcon>
+      </VAppBarNavIcon>
+      <div class="d-none d-md-flex">
+        <VBtn
+          v-for="(tab, i) in tabs"
+          :key="i"
+          variant="plain"
+          class="font-weight-bold text-body-1 text-black"
+          nuxt
+          :to="tab.path"
+        >
+          {{ tab.title }}
+        </VBtn>
+      </div>
+      <VSpacer class="d-none d-md-flex" />
+      <div class="d-none d-md-flex">
+        <VBtn
+          height="100"
+          variant="text"
+          class="font-weight-bold text-body-2"
+          color="primary"
+        >
+          Login
+        </VBtn>
+        <VBtn
+          variant="flat"
+          height="100"
+          class="font-weight-bold text-body-2"
+          color="primary"
+          >Sign Up</VBtn
+        >
+      </div>
+    </VAppBar>
+
+    <!-- Navigation Drawer -->
+    <VNavigationDrawer v-model="drawer" position="fixed" temporary>
+      <VList nav dense>
+        <VListItem
+          v-for="(tab, i) in tabs"
+          :key="i"
+          :title="tab.title"
+          nuxt
+          :to="tab.path"
+        ></VListItem>
+      </VList>
+    </VNavigationDrawer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const drawer = ref(false);
+const tabs = reactive([
+  { path: "/", title: "Home" },
+  { path: "/how-it-works", title: "How It Works" },
+  { path: "/benefits", title: "Benefits" },
+  { path: "/opportunities", title: "Opportunities" },
+  { path: "/about", title: "About Us" },
+  { path: "/contact", title: "Contact" },
+]);
+</script>
+
+<style scoped>
+.btn {
+  text-transform: unset !important;
+}
+.v-btn--active{
+  color: #007ACC !important;
+}
+</style>
