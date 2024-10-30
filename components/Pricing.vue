6423a4fefc869454559b828ec4fcfc70e57b2558
Steto Javellana <javellanasteto99@gmail.com>
2024-10-30 22:44:15 +0800

    layout for information

--- components/Pricing.vue
+++ components/Pricing.vue
@@ -1,7 +1,15 @@
 <template>
     <section id="pricingSection" style="background: transparent;">
         <VCard>
-            <VCardText>Hello</VCardText>
+             <v-sheet class="d-flex flex-wrap ">
+    <v-sheet class="flex-1-0 ma-2 pa-2">
+      I'm an element in an inline flexbox container!
+    </v-sheet>
+    <VDivider vertical></VDivider>
+    <v-sheet class="flex-1-1 ma-2 pa-2">
+      I'm a single element in an inline flexbox container!
+    </v-sheet>
+  </v-sheet>
         </VCard>
     </section>
 </template>
