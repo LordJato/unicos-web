6116a88a9c1f61662e8089192a1030f6378ce13c
Steto Javellana <javellanasteto99@gmail.com>
2024-11-16 22:33:29 +0800

    add component

--- pages/index.vue
+++ pages/index.vue
@@ -27,69 +27,7 @@
     </div>
     <!-- About -->
     <div id="about">
-      <VContainer >
-        <VRow align="stretch" justify="space-between">
-          <VCol cols="12" md="4">
-            <VCard height="100%"
-              image="https://images.squarespace-cdn.com/content/v1/5cfb0f8783523500013c5639/1c0249f2-14ba-49f5-94c9-14b82e95c356/Professional-headshots-vancouver--20.jpg">
-              <VCardItem>
-                <VCardTitle>Tenant or Company</VCardTitle>
-                <VCardSubtitle>UNICOS: Simplify HR management across your organization.</VCardSubtitle>
-              </VCardItem>
-              <VCardText>
-                <div>
-                  As a company or tenant, you can:
-                </div>
-                <p>Manage multiple companies under one account</p>
-                <p>Post jobs, hire skilled workers, and manage applications</p>
-                <p>Access HRIS, payroll, and employee self-service portals</p>
-              </VCardText>
-              <VCardActions>
-                <v-btn append-icon="mdi-chevron-right" color="red-lighten-2" text="Book Activity" variant="outlined"
-                  block></v-btn>
-              </VCardActions>
-            </VCard>
-          </VCol>
-          <VCol cols="12" md="4">
-            <VCard height="100%" image="https://freedesignfile.com/upload/2017/04/Smile-job-seekers-HD-picture.jpg">
-              <VCardItem>
-                <VCardTitle>Job Seeker</VCardTitle>
-                <VCardSubtitle>Find Your Dream Job or Client with UNICOS</VCardSubtitle>
-              </VCardItem>
-              <VCardText>
-                <div>As a job seeker or worker, unlock opportunities with UNICOS:</div>
-                <p> Search and apply to job openings from various clients and companies</p>
-                <p>Highlight your expertise and showcase your portfolio to attract top employers and land your dream
-                  job.</p>
-                <p>Find temporary or project-based work with leading clients.</p>
-              </VCardText>
-              <VCardActions>
-                <v-btn append-icon="mdi-chevron-right" color="red-lighten-2" text="Book Activity" variant="outlined"
-                  block></v-btn>
-              </VCardActions>
-            </VCard>
-          </VCol>
-          <VCol cols="12" md="4">
-            <VCard height="100%"
-              image="https://welovesalt.com/my/wp-content/uploads/sites/13/2021/12/my-human-resources-recruitment-agency.png">
-              <VCardItem>
-                <VCardTitle>Client</VCardTitle>
-                <VCardSubtitle>Find Your Perfect Match with UNICOS</VCardSubtitle>
-              </VCardItem>
-              <VCardText>
-                As a client, find skilled workers tailored to your needs:
-                Search our vast pool of qualified candidates
-                Filter by skills, experience, and location
-                Connect with top talent for your business needs.
-              </VCardText>
-              <VCardActions>
-                <v-btn append-icon="mdi-chevron-right" color="red-lighten-2" text="Book Activity" variant="outlined"
-                  block></v-btn>
-              </VCardActions>
-            </VCard>
-          </VCol>
-        </VRow>
-      </VContainer>
+      <HomeAbout />
     </div>
 
     <!-- Hire Now -->
