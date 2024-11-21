aa4657fdeaf42042f85f5babcf83a3de56c0e651
Steto Javellana <javellanasteto99@gmail.com>
2024-11-21 21:30:44 +0800

    down with partners feedback

--- components/Home/PartnersFeedback.vue
+++ components/Home/PartnersFeedback.vue
@@ -2,47 +2,41 @@
     <section id="homeFeedback">
         <div class="w-100 text-center">
             <h1 class="text-h4">Voices from Our Community</h1>
-            <p class="text-h6 mt-4 mb-8"> We're proud of the results we've achieved together. Here's what our clients
+            <p class="text-h6 mt-4 mb-10"> We're proud of the results we've achieved together. Here's what our clients
                 say about working with us </p>
-            <v-carousel hide-delimiters show-arrows height="100%">
-                <v-carousel-item color="transparent">
-                    <VCard max-width="1080" class="position-relative rounded-xl mx-auto" elevation="9">
-                        <VImg src="/public/img/index/business.jpg" cover>
+            <v-carousel hide-delimiters show-arrows class="h-100">
+                <v-carousel-item color="transparent" v-for="(feedback, i) in feedbacks">
+                    <VCard  max-width="1100" height="550" class=" rounded-xl mx-auto" elevation="9">
+                        <VImg :src="feedback.backgroundImg" cover class="position-relative">
                             <div class="overlay"></div>
                             <VContainer class="h-100 text-start pa-16">
-                                <VRow>
-                                    <v-col cols="12" md="7">
-                                        <v-icon icon="mdi-format-quote-open-outline" size="100"
-                                            color="secondary"></v-icon>
+                                <VRow class="h-100">
+                                    <VCol cols="12" md="7" class="d-flex align-start flex-column">
+                                        <VIcon icon="mdi-format-quote-open-outline" size="100" color="secondary" />
                                         <p class="text-subtitle-1 text-justify mt-2">
-                                            Unicoss has been our trusted partner for over five years, providing top-tier
-                                            development resources through their team of 40+ skilled professionals. Under
-                                            Steto's leadership, they've consistently demonstrated exceptional quality,
-                                            loyalty, and commitment.
+                                            {{ feedback.message }}
                                         </p>
-                                        <p class="mt-8">
-                                            We strongly recommend Unicoss for their outstanding expertise and service.
+                                        <p class="mt-8" v-if="feedback.subMessage">
+                                            {{ feedback.subMessage }}
                                         </p>
-                                        <div class="d-flex align-center mt-10">
+                                        <div class="d-flex align-center mt-auto">
                                             <span class="mr-3">
-                                                <v-avatar size="large">
-                                                    <v-img alt="John" style="filter: grayscale(100%);"
-                                                        src="https://imgix.ranker.com/user_node_img/50082/1001639646/original/being-strong-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=355" />
-                                                </v-avatar>
+                                                <VAvatar size="large">
+                                                    <VImg :alt="feedback.name" style="filter: grayscale(100%);"
+                                                        :src="feedback.img" />
+                                                </VAvatar>
                                             </span>
                                             <span>
-                                                <div class="text-body-1 font-weight-bold">Steto Javellana</div>
-                                                <div class="text-caption">Junior Software Developer</div>
+                                                <div class="text-body-1 font-weight-bold">{{ feedback.name }}</div>
+                                                <div class="text-caption">{{ feedback.position }}</div>
                                             </span>
                                         </div>
-                                    </v-col>
+                                    </VCol>
                                 </VRow>
                             </VContainer>
                         </VImg>
                     </VCard>
-
                 </v-carousel-item>
-
                 <template v-slot:prev="{ props }">
                     <v-btn color="secondary" variant="elevated" @click="props.onClick"
                         prepend-icon="mdi-page-previous-outline" class="text-white text-caption">Prev</v-btn>
@@ -58,6 +52,38 @@
 </template>
 
 <script setup lang="ts">
+
+const feedbacks = ref([
+    {
+        backgroundImg: '/img/index/business.jpg',
+        message: "Unicos has been our trusted partner for over five years, providing top-tier development resources through their team of 40+ skilled professionals. Under Steto's leadership, they've consistently demonstrated exceptional quality,loyalty, and commitment.",
+        subMessage: "We strongly recommend Unicos for their outstanding expertise and service.",
+        img: "https://imgix.ranker.com/user_node_img/50082/1001639646/original/being-strong-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=355",
+        name: 'Steto Javellana',
+        position: 'Junior Software Developer'
+    },
+
+    {
+        backgroundImg: "https://img.freepik.com/premium-photo/business-woman-sitting-front-tablet-laptop-computer-with-financial-graphs-statistics-monitor-officexa_71455-2606.jpg",
+        message: "Everything is hard in the beginning. It doesn’t matter how vast the sea or how profound the idea, there is always an origin. One step at a time to walk a thousand miles. Start from the bottom with the most sincere attitude towards the top. If it doesn’t work the first time, do it a second time. If you fail again, then go for the third. As long as you persevere, you will see results.",
+        subMessage: "We highly praise Unicos for their exceptional expertise and commitment to excellence.",
+        img: "https://i.pinimg.com/736x/11/aa/57/11aa571334382cab1e7e60d47b7a755a.jpg",
+        name: 'Li Qiye',
+        position: "Emperor's Domination"
+    },
+
+    {
+        backgroundImg: "https://img.freepik.com/premium-photo/young-man-connecting-with-digital-tablet-home_738298-156.jpg",
+        message: "Continue living, continue preserving. To us minor characters, crying is of no use, no one will hear us even if we shout, we can only persevere by ourselves.",
+        subMessage: "Persevere until you accomplish great feats, become extraordinary.",
+        img: "https://static1.personality-database.com/profile_images/75301bd9818c41ecb2b9690599629885.png",
+        name: 'Fang Yuan',
+        position: 'Reverened Insanity'
+    },
+
+
+    
+])
 
 </script>
 
@@ -80,7 +106,7 @@
     left: 0;
     right: 0;
     height: 100%;
-    background: linear-gradient(90deg, rgba(7, 15, 43, 1) 15%, rgba(7, 15, 43, 0.9) 70%, rgba(33, 50, 104, 0.9) 100%);
+    background: linear-gradient(90deg, rgba(7, 15, 43, 1) 25%, rgba(7, 15, 43, 0.9) 70%, rgba(33, 50, 104, 0.9) 100%);
     z-index: -1;
 }
 </style>
