0ea7515bbb88ba4fa3dfbd5c7f51e3975fe18429
Steto Javellana <javellanasteto99@gmail.com>
2024-11-22 23:35:27 +0800

    add cards

--- components/Home/TopWorkers.vue
+++ components/Home/TopWorkers.vue
@@ -8,7 +8,62 @@
                             <h1 class="text-h4">Partner with dedicated professionals who deliver excellence with loyalty and commitment.</h1>
                         </VCol>
                         <VCol cols="12" md="7" class="d-flex justify-center align-center">
-                  
+                            <v-card
+    class="mx-auto"
+    prepend-icon="$vuetify"
+    subtitle="The #1 Vue UI Library"
+    width="400"
+  >
+    <template v-slot:title>
+      <span class="font-weight-black">Welcome to Vuetify</span>
+    </template>
+
+    <v-card-text class="bg-surface-light pt-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!
+    </v-card-text>
+  </v-card>
+  <v-card
+    class="mx-auto"
+    prepend-icon="$vuetify"
+    subtitle="The #1 Vue UI Library"
+    width="400"
+  >
+    <template v-slot:title>
+      <span class="font-weight-black">Welcome to Vuetify</span>
+    </template>
+
+    <v-card-text class="bg-surface-light pt-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!
+    </v-card-text>
+  </v-card>
+  <v-card
+    class="mx-auto"
+    prepend-icon="$vuetify"
+    subtitle="The #1 Vue UI Library"
+    width="400"
+  >
+    <template v-slot:title>
+      <span class="font-weight-black">Welcome to Vuetify</span>
+    </template>
+
+    <v-card-text class="bg-surface-light pt-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!
+    </v-card-text>
+  </v-card>
+  <v-card
+    class="mx-auto"
+    prepend-icon="$vuetify"
+    subtitle="The #1 Vue UI Library"
+    width="400"
+  >
+    <template v-slot:title>
+      <span class="font-weight-black">Welcome to Vuetify</span>
+    </template>
+
+    <v-card-text class="bg-surface-light pt-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!
+    </v-card-text>
+  </v-card>
                         </VCol>
                     </VRow>
                 </VContainer>
