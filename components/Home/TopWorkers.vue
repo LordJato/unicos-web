f1bdb7bafad595c99aa53ec5ade6c2d63a14f21e
Steto Javellana <javellanasteto99@gmail.com>
2024-11-22 23:33:24 +0800

    change card layout

--- components/Home/TopWorkers.vue
+++ components/Home/TopWorkers.vue
@@ -8,28 +8,7 @@
                             <h1 class="text-h4">Partner with dedicated professionals who deliver excellence with loyalty and commitment.</h1>
                         </VCol>
                         <VCol cols="12" md="7" class="d-flex justify-center align-center">
-                            <VCard class="mx-auto pa-6 pb-8" max-width="448" rounded="lg">
-                                <div class="text-subtitle-1 text-medium-emphasis">Name</div>
-
-                                <VTextField density="compact" prepend-inner-icon="mdi-account" variant="outlined">
-                                </VTextField>
-
-
-                                <div class="text-subtitle-1 text-medium-emphasis">Email</div>
-
-                                <VTextField density="compact" prepend-inner-icon="mdi-email-outline" variant="outlined">
-                                </VTextField>
-
-                                <div class="text-subtitle-1 text-medium-emphasis">Message</div>
-
-                                <VTextarea density="compact" prepend-inner-icon="mdi-message" variant="outlined">
-                                </VTextarea>
-
-
-                                <VBtn color="secondary" class="text-white" append-icon="mdi-email-fast" block>
-                                    Send Email
-                                </VBtn>
-                            </VCard>
+                  
                         </VCol>
                     </VRow>
                 </VContainer>
