20f73637596f2be6f3370e4343d565cd802e7df4
Steto Javellana <javellanasteto99@gmail.com>
2024-11-23 22:01:39 +0800

    add content

--- components/Home/TopWorkers.vue
+++ components/Home/TopWorkers.vue
@@ -5,13 +5,57 @@
         <VContainer class="h-100">
           <VRow justify="center" align="center">
             <VCol cols="12" md="6">
-              <h1 class="text-h4">Meet Our Top Achievers!</h1>
-              <p>Leading the charge in innovation and excellence. Explore their profiles, learn from their expertise,
-                and collaborate with the best on Unicos.</p>
+              <h1 class="text-h4 mb-4">Meet Our Top Achievers!</h1>
+              <h3 class="text-h6 mb-4">Leading the charge in innovation and excellence. Explore their profiles, learn
+                from their expertise,
+                and collaborate with the best on Unicos.</h3>
+              <div class="d-flex mb-2">
+                <v-icon color="secondary" icon="mdi-tag-check-outline" class="mr-2" size="small"></v-icon>
+                <p>
+                  Professional certifications
+                </p>
+              </div>
+              <div class="d-flex mb-2">
+                <v-icon color="secondary" icon="mdi-tag-check-outline" class="mr-2" size="small"></v-icon>
+                <p>
+                  Portfolio showcasing expertise
+                </p>
+              </div>
+
+              <div class="d-flex mb-2">
+                <v-icon color="secondary" icon="mdi-tag-check-outline" class="mr-2" size="small"></v-icon>
+                <p>
+                  High-quality deliverables
+                </p>
+              </div>
+              <div class="d-flex mb-2">
+                <v-icon color="secondary" icon="mdi-tag-check-outline" class="mr-2" size="small"></v-icon>
+                <p>
+                  Adherence to deadlines
+                </p>
+              </div>
+              <div class="d-flex mb-2">
+                <v-icon color="secondary" icon="mdi-tag-check-outline" class="mr-2" size="small"></v-icon>
+                <p>
+                  Clear and concise reporting
+                </p>
+              </div>
+              <div class="d-flex mb-2">
+                <v-icon color="secondary" icon="mdi-tag-check-outline" class="mr-2" size="small"></v-icon>
+                <p>
+                  Long-term collaboration potential
+                </p>
+              </div>
+              <div class="d-flex mb-2">
+                <v-icon color="secondary" icon="mdi-tag-check-outline" class="mr-2" size="small"></v-icon>
+                <p>
+                  Strong work ethic
+                </p>
+              </div>
             </VCol>
             <VCol cols="12" md="6" class="d-flex justify-center align-center">
               <VRow justify="center" align="center">
-                <VCol cols="12" md="4" class=" w-100">
+                <VCol cols="12" md="6" class="mb-md-16">
                   <VCard class="mb-4 text-center rounded-xl pa-8" color="#0b1420">
                     <VImg class="rounded-circle mx-auto" cover height="120" width="120"
                       src="https://rszr.getimg.ai/resize?url=https%3A%2F%2Fimg.getimg.ai%2Fgenerated%2Fimg-mGtSk9modTnvsDVsxaXZC.jpeg&type=auto&width=640&speed=5">
@@ -20,7 +64,7 @@
                     <VCardSubtitle>Software Developer</VCardSubtitle>
                     <VCardText>Content for card 1</VCardText>
                   </VCard>
-                  <VCard class="mb-4 text-center rounded-xl pa-8" color="#0b1420">
+                  <VCard class="mb-3 text-center rounded-xl pa-8" color="#0b1420">
                     <VImg class="rounded-circle mx-auto" cover height="120" width="120"
                       src="https://rszr.getimg.ai/resize?url=https%3A%2F%2Fimg.getimg.ai%2Fgenerated%2Fimg-mGtSk9modTnvsDVsxaXZC.jpeg&type=auto&width=640&speed=5">
                     </VImg>
@@ -29,8 +73,8 @@
                     <VCardText>Content for card 1</VCardText>
                   </VCard>
                 </VCol>
-                <VCol cols="12" md="4" class="d-flex align-center">
-                    <VCard class="mb-4 text-center rounded-xl pa-8 w-100" color="#0b1420" >
+                <VCol cols="12" md="6" class="mt-md-16">
+                  <VCard class="mb-4 text-center rounded-xl pa-8 w-100" color="#0b1420">
                     <VImg class="rounded-circle mx-auto" cover height="120" width="120"
                       src="https://rszr.getimg.ai/resize?url=https%3A%2F%2Fimg.getimg.ai%2Fgenerated%2Fimg-mGtSk9modTnvsDVsxaXZC.jpeg&type=auto&width=640&speed=5">
                     </VImg>
@@ -38,7 +82,14 @@
                     <VCardSubtitle>Software Developer</VCardSubtitle>
                     <VCardText>Content for card 1</VCardText>
                   </VCard>
-                 
+                  <VCard class="mb-4 text-center rounded-xl pa-8 w-100" color="#0b1420">
+                    <VImg class="rounded-circle mx-auto" cover height="120" width="120"
+                      src="https://rszr.getimg.ai/resize?url=https%3A%2F%2Fimg.getimg.ai%2Fgenerated%2Fimg-mGtSk9modTnvsDVsxaXZC.jpeg&type=auto&width=640&speed=5">
+                    </VImg>
+                    <VCardTitle>Steto Javellana</VCardTitle>
+                    <VCardSubtitle>Software Developer</VCardSubtitle>
+                    <VCardText>Content for card 1</VCardText>
+                  </VCard>
                 </VCol>
               </VRow>
             </VCol>
