a00b5527776084c93bb9c44d1746b06c51d136cf
Steto Javellana <javellanasteto99@gmail.com>
2024-11-23 17:59:32 +0800

    add cards

--- components/Home/TopWorkers.vue
+++ components/Home/TopWorkers.vue
@@ -1,82 +1,56 @@
 <template>
-    <section>
-        <VRow justify="center" align="center" class="h-100">
-            <VCol cols="12" lg="9">
-                <VContainer class="h-100">
-                    <VRow justify="center" align="center">
-                        <VCol cols="12" md="5">
-                            <h1 class="text-h4">Meet Our Top Achievers!</h1>
-                            <p>Leading the charge in innovation and excellence. Explore their profiles, learn from their expertise, and collaborate with the best on Unicos.</p>
-                        </VCol>
-                        <VCol cols="12" md="7" class="d-flex justify-center align-center">
-                            <v-card
-    class="mx-auto"
-    prepend-icon="$vuetify"
-    subtitle="The #1 Vue UI Library"
-    width="400"
-  >
-    <template v-slot:title>
-      <span class="font-weight-black">Welcome to Vuetify</span>
-    </template>
-
-    <v-card-text class="bg-surface-light pt-4">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!
-    </v-card-text>
-  </v-card>
-  <v-card
-    class="mx-auto"
-    prepend-icon="$vuetify"
-    subtitle="The #1 Vue UI Library"
-    width="400"
-  >
-    <template v-slot:title>
-      <span class="font-weight-black">Welcome to Vuetify</span>
-    </template>
-
-    <v-card-text class="bg-surface-light pt-4">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!
-    </v-card-text>
-  </v-card>
-  <v-card
-    class="mx-auto"
-    prepend-icon="$vuetify"
-    subtitle="The #1 Vue UI Library"
-    width="400"
-  >
-    <template v-slot:title>
-      <span class="font-weight-black">Welcome to Vuetify</span>
-    </template>
-
-    <v-card-text class="bg-surface-light pt-4">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!
-    </v-card-text>
-  </v-card>
-  <v-card
-    class="mx-auto"
-    prepend-icon="$vuetify"
-    subtitle="The #1 Vue UI Library"
-    width="400"
-  >
-    <template v-slot:title>
-      <span class="font-weight-black">Welcome to Vuetify</span>
-    </template>
-
-    <v-card-text class="bg-surface-light pt-4">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!
-    </v-card-text>
-  </v-card>
-                        </VCol>
-                    </VRow>
-                </VContainer>
+  <section>
+    <VRow justify="center" align="center" class="h-100">
+      <VCol cols="12" lg="9">
+        <VContainer class="h-100">
+          <VRow justify="center" align="center">
+            <VCol cols="12" md="6">
+              <h1 class="text-h4">Meet Our Top Achievers!</h1>
+              <p>Leading the charge in innovation and excellence. Explore their profiles, learn from their expertise,
+                and collaborate with the best on Unicos.</p>
             </VCol>
-        </VRow>
-    </section>
+            <VCol cols="12" md="6" class="d-flex justify-center align-center">
+              <VRow justify="center" align="center">
+                <VCol cols="12" md="4" class=" w-100">
+                  <VCard class="mb-4 text-center rounded-xl pa-8" color="#0b1420">
+                    <VImg class="rounded-circle mx-auto" cover height="120" width="120"
+                      src="https://rszr.getimg.ai/resize?url=https%3A%2F%2Fimg.getimg.ai%2Fgenerated%2Fimg-mGtSk9modTnvsDVsxaXZC.jpeg&type=auto&width=640&speed=5">
+                    </VImg>
+                    <VCardTitle>Steto Javellana</VCardTitle>
+                    <VCardSubtitle>Software Developer</VCardSubtitle>
+                    <VCardText>Content for card 1</VCardText>
+                  </VCard>
+                  <VCard class="mb-4 text-center rounded-xl pa-8" color="#0b1420">
+                    <VImg class="rounded-circle mx-auto" cover height="120" width="120"
+                      src="https://rszr.getimg.ai/resize?url=https%3A%2F%2Fimg.getimg.ai%2Fgenerated%2Fimg-mGtSk9modTnvsDVsxaXZC.jpeg&type=auto&width=640&speed=5">
+                    </VImg>
+                    <VCardTitle>Steto Javellana</VCardTitle>
+                    <VCardSubtitle>Software Developer</VCardSubtitle>
+                    <VCardText>Content for card 1</VCardText>
+                  </VCard>
+                </VCol>
+                <VCol cols="12" md="4" class="d-flex align-center">
+                    <VCard class="mb-4 text-center rounded-xl pa-8 w-100" color="#0b1420" >
+                    <VImg class="rounded-circle mx-auto" cover height="120" width="120"
+                      src="https://rszr.getimg.ai/resize?url=https%3A%2F%2Fimg.getimg.ai%2Fgenerated%2Fimg-mGtSk9modTnvsDVsxaXZC.jpeg&type=auto&width=640&speed=5">
+                    </VImg>
+                    <VCardTitle>Steto Javellana</VCardTitle>
+                    <VCardSubtitle>Software Developer</VCardSubtitle>
+                    <VCardText>Content for card 1</VCardText>
+                  </VCard>
+                 
+                </VCol>
+              </VRow>
+            </VCol>
+          </VRow>
+        </VContainer>
+      </VCol>
+    </VRow>
+  </section>
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
