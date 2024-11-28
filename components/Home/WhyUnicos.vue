e5871a63b0d94e9ebd7831a85042deeb9fc1d110
Steto Javellana <javellanasteto99@gmail.com>
2024-11-28 23:52:29 +0800

    add lazy load

--- components/Home/WhyUnicos.vue
+++ components/Home/WhyUnicos.vue
@@ -1,23 +1,33 @@
 <template>
-    <section id="whyUnicos">
-        <video autoplay muted loop class="background-video">
-      <source src="/public/img/index/why-us-bg.mp4" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
+    <section id="whyUnicos" v-intersect="onIntersect">
+            <video v-if="isIntersecting" autoplay muted loop class="background-video">
+                <source :src="videoSrc" type="video/mp4">
+            </video>
+
     </section>
 </template>
 
 <script setup lang="ts">
 
+import videoBg from '@/public/img/index/why-us-bg.mp4'
+
+
+const isIntersecting = ref(false)
+
+const onIntersect = (isIntersectingValue: any) => {
+    isIntersecting.value = isIntersectingValue
+}
+const videoSrc = videoBg
+
 </script>
 
 <style scoped>
 .background-video {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
 }
 </style>
