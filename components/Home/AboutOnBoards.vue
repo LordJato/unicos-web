e97bcf2bdb7f6673756bd3601c43e8da6366c5af
Steto Javellana <javellanasteto99@gmail.com>
2024-11-28 18:01:09 +0800

    add layout for both on boards and tenancy

--- components/Home/AboutOnBoards.vue
+++ components/Home/AboutOnBoards.vue
@@ -1,6 +1,10 @@
 <template>
     <section id="aboutOnBoards">
-      Our cloud-based applications employ a secured multitenancy architecture, enabling universal access and unparalleled data security. With cutting-edge encryption and logical data segregation, our users enjoy:
+
+      <VRow justify="center" align="center" class="h-100">
+        
+          <VCol cols="12" md="6">
+            Our cloud-based applications employ a secured multitenancy architecture, enabling universal access and unparalleled data security. With cutting-edge encryption and logical data segregation, our users enjoy:
 Universal Accessibility: Secure access from anywhere, on any device.
 Data Confidentiality: Each tenant's data is safeguarded from unauthorized access.
 Scalability: Elastic infrastructure adapts to evolving demands.
@@ -12,6 +16,14 @@ Key Milestones:
 300+ Companies Onboarded: Industry leaders partner with us.
 Join Our Growing Ecosystem!
 Experience secure, scalable, and reliable cloud-based solutions. Ideal for businesses seeking efficient, cost-effective, and data-driven operations.
+          </VCol>
+          <VCol cols="12" md="6">
+          <VImg src="/public/img/index/about-on-boards-bg.png" cover />
+          </VCol>
+      </VRow>
+
+      
+
     </section>
 </template>
 
@@ -30,7 +42,7 @@ Experience secure, scalable, and reliable cloud-based solutions. Ideal for busin
       rgba(0, 0, 0, 0.5) 11.2%,
       rgba(63, 61, 61, 0.5) 78.9%
     ),
-    url("/img/index/about-on-boards-bg.png") no-repeat left ;
+    url("") no-repeat left ;
 }
 
 </style>
--- components/Home/AboutTenancy.vue
+++ components/Home/AboutTenancy.vue
@@ -1,14 +1,29 @@
 <template>
     <section>
+
+        <VRow justify="center" align="center" class="h-100">
+
+            <VCol cols="12" md="6">
+                <VImg src="/public/img/index/about-tenancy.png" cover />
+
+            </VCol>
+            <VCol cols="12" md="6">
+
+                <div>
+                    Secured Data Multitenancy Statement
+                    Our web applications are designed with multitenancy architecture, ensuring the highest level of data
+                    security and isolation. Each tenant's data is logically segregated and encrypted, providing a secure
+                    and reliable environment for our users. Our multitenancy approach ensures:
+                    Data Confidentiality: Each tenant's data is accessible only to authorized personnel.
+                    Data Integrity: Regular backups and versioning prevent data loss or tampering.
+                    Data Availability: Scalable infrastructure ensures high uptime and accessibility.
+                    Compliance: Adherence to international data protection standards and regulations.</div>
+            </VCol>
+        </VRow>
+
         <VImg src="/img/index/about-tenancy.png"></VImg>
 
-        <div>
-Secured Data Multitenancy Statement
-Our web applications are designed with multitenancy architecture, ensuring the highest level of data security and isolation. Each tenant's data is logically segregated and encrypted, providing a secure and reliable environment for our users. Our multitenancy approach ensures:
-Data Confidentiality: Each tenant's data is accessible only to authorized personnel.
-Data Integrity: Regular backups and versioning prevent data loss or tampering.
-Data Availability: Scalable infrastructure ensures high uptime and accessibility.
-Compliance: Adherence to international data protection standards and regulations.</div>
+
     </section>
 </template>
 
@@ -16,6 +31,4 @@ Compliance: Adherence to international data protection standards and regulations
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
