a32e2ec07ef335c676321453a33d2b7a18701a38
Steto Javellana <javellanasteto99@gmail.com>
2024-11-29 23:45:45 +0800

    fix on boards

--- components/Home/AboutOnBoards.vue
+++ components/Home/AboutOnBoards.vue
@@ -1,10 +1,12 @@
 <template>
     <section id="aboutOnBoards">
-
-      <VRow justify="center" align="center" class="h-100">
+      <VRow justify="end" align="center" class="h-100">
+            <VCol cols="12" lg="9">
+                <VContainer class="h-100">
+                  <VRow justify="center" align="center" class="h-100">
         
-          <VCol cols="12" md="6">
-            Our cloud-based applications employ a secured multitenancy architecture, enabling universal access and unparalleled data security. With cutting-edge encryption and logical data segregation, our users enjoy:
+        <VCol cols="12" md="6">
+          Our cloud-based applications employ a secured multitenancy architecture, enabling universal access and unparalleled data security. With cutting-edge encryption and logical data segregation, our users enjoy:
 Universal Accessibility: Secure access from anywhere, on any device.
 Data Confidentiality: Each tenant's data is safeguarded from unauthorized access.
 Scalability: Elastic infrastructure adapts to evolving demands.
@@ -16,11 +18,16 @@ Key Milestones:
 300+ Companies Onboarded: Industry leaders partner with us.
 Join Our Growing Ecosystem!
 Experience secure, scalable, and reliable cloud-based solutions. Ideal for businesses seeking efficient, cost-effective, and data-driven operations.
-          </VCol>
-          <VCol cols="12" md="6">
-          <VImg src="/public/img/index/about-on-boards-bg.png" cover />
-          </VCol>
+        </VCol>
+        <VCol cols="12" md="6">
+        <VImg src="/public/img/index/about-on-boards-bg.png" cover />
+        </VCol>
+    </VRow>
+                </VContainer>
+            </VCol>
       </VRow>
+
+   
 
       
 
